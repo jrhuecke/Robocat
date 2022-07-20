cc3c449f8ec556b30cd189e7844519e33201ffcd
Ryan Hueckel <jrhuecke@ucsc.edu>
2022-07-20 14:33:11 -0700

    Changed movement so it is acceleration based now, jump is currently bad because of that though. Also fixed bug with player sticking to walls

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -15,16 +15,19 @@ public class PlayerMovement : MonoBehaviour
         WALL_JUMPING,
         EXPLODING,
     }
-    [SerializeField] private float speed;
+    [SerializeField] private float maxSpeed;
+    [SerializeField] private float acceleration;
+    [SerializeField] private float decelartion;
     [SerializeField] private float jumpSpeed;
+    [SerializeField] private float jumpBuffer;
     [SerializeField] private LayerMask groundLayer;
-    [SerializeField] public float jumpBuffer;
     private float jumpBufferTimer;
     private Rigidbody2D body;
     private Animator anim;
     private BoxCollider2D playerCollider;
     private State state;
     private float horizontalInput;
+    private bool changingDirection;
 
 
     private void Awake()
@@ -37,14 +40,15 @@ public class PlayerMovement : MonoBehaviour
     
     private void Update()
     {
-        //Stores current left/right arrow input as a num between -1 and 1 where -1 is left, 0 is nothing, and 1 is right
-        horizontalInput = Input.GetAxis("Horizontal");
+        //Stores current left/right arrow input as: -1, 0, or 1 where -1 is left, 0 is nothing, and 1 is right
+        horizontalInput = Input.GetAxisRaw("Horizontal");
 
         //jump buffer (stores jump so that they can press jump right before they hit the ground)
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
             jumpBufferTimer = jumpBuffer;
-        } else
+        } 
+        else
         {
             if (jumpBufferTimer >= 0)
             {
@@ -60,13 +64,16 @@ public class PlayerMovement : MonoBehaviour
         else if (horizontalInput < -0.01f)
         {
             transform.localScale = new Vector3(-4, 4, 4);
-        }
+        }   
+    }
 
+    private void FixedUpdate()
+    {
         switch (state)
         {
             case State.STANDING:
                 //checks if player is starting to jump
-                if (jumpBufferTimer > 0)
+                if (jumpBufferTimer > 0 && onGround())
                 {
                     state = State.JUMPING;
                     jumpBufferTimer = 0;
@@ -81,13 +88,15 @@ public class PlayerMovement : MonoBehaviour
                     state = State.RUNNING;
                     anim.SetBool("Standing", false);
                     anim.SetBool("Running", true);
-                    body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
+                    MovePlayer();
+                    ApplyDrag();
                     print(state);
                 }
                 break;
 
             case State.RUNNING:
-                body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
+                MovePlayer();
+                ApplyDrag();
                 //checks if player has stopped moving. Checks if left/right are being pressed down on top of horizontalinput
                 //so that the standing animation doesnt play when changing directions while running
                 if (!Input.GetKey(KeyCode.LeftArrow) && !Input.GetKey(KeyCode.RightArrow) && horizontalInput == 0)
@@ -98,7 +107,7 @@ public class PlayerMovement : MonoBehaviour
                     print(state);
                 }
                 //checks if player is starting to jump
-                else if (jumpBufferTimer > 0)
+                else if (jumpBufferTimer > 0 && onGround())
                 {
                     state = State.JUMPING;
                     jumpBufferTimer = 0;
@@ -110,7 +119,8 @@ public class PlayerMovement : MonoBehaviour
                 break;
 
             case State.JUMPING:
-                body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
+                MovePlayer();
+                ApplyDrag();
                 //checks if player is back on the ground, also checks velocity that way it doesnt trigger on the first frames of the jump
                 if (onGround() && body.velocity.y <= 0)
                 {
@@ -122,10 +132,44 @@ public class PlayerMovement : MonoBehaviour
                 break;
         }
     }
+
+    private void MovePlayer()
+    {
+        body.AddForce(new Vector2(horizontalInput, 0f) * acceleration);
+        
+        if (Mathf.Abs(body.velocity.x) > maxSpeed)
+        {
+            body.velocity = new Vector2(Mathf.Sign(body.velocity.x) * maxSpeed, body.velocity.y);
+        }
+    }
+
+    private void ApplyDrag()
+    {
+        //checks if the player is currently trying to change directions by comparing player velocity to current input
+        if ((body.velocity.x > 0f && horizontalInput < 0f) || (body.velocity.y < 0f && horizontalInput > 0f))
+        {
+            changingDirection = true;
+        }
+        else
+        {
+            changingDirection = false;
+        }
+        
+        //applies drag when the player is not inputting left/right or when the player is trying to change directions
+        if (Mathf.Abs(horizontalInput) < 1f || changingDirection)
+        {
+            body.drag = decelartion;
+        }
+        else
+        {
+            body.drag = 0f;
+        }
+    }
+
     private bool onGround()
     {
-        //returns null if there is no collider found 0.1f below player
-        RaycastHit2D raycastHit = Physics2D.BoxCast(playerCollider.bounds.center, playerCollider.bounds.size, 0, Vector2.down, 0.1f, groundLayer);
+        //returns null if there is no collider found 0.05f below player
+        RaycastHit2D raycastHit = Physics2D.BoxCast(playerCollider.bounds.center, playerCollider.bounds.size, 0, Vector2.down, 0.05f, groundLayer);
         return raycastHit.collider != null;
     }
 }
