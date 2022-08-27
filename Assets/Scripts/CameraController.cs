1c4936bdc124fe6606063e773eb17161a68d9e83
Ryan Hueckel <jrhuecke@ucsc.edu>
2022-08-26 22:04:52 -0700

    Added camera controller that follows a little ahead of the playe

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private float cameraSpeed;
+    [SerializeField] private float aheadDistance;
+    [SerializeField] private Transform player;
+    private float lookAhead;
+
+    private void Update()
+    {
+        transform.position = new Vector3(player.position.x + lookAhead, transform.position.y, transform.position.z);
+        lookAhead = Mathf.Lerp(lookAhead, (aheadDistance * player.localScale.x), Time.deltaTime * cameraSpeed);
+    }
+}
